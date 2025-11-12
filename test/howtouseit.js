55c72b1573395302adda165a32ae871baefaad97
Mani Arasteh <mani.arasteh92@gmail.com>
2025-11-12 19:44:31 +0330

    Update howtouseit.js

--- test/howtouseit.js
+++ test/howtouseit.js
@@ -1,9 +1,26 @@
-// You can use Edcore like that:
-const { Editor } = require("../edcore.node");
+// howtouseit.js
 
-// Make a new variable using the Editor constructor
-const editor = new Editor();
-// Insert some text at the beginning of the editor
-editor.insert_text(0, "Hello from a program using Edcore!");
-// Print the result
-console.log(editor.get_line(0));
+// Import the native module built by napi-rs
+// When published, users would just: import { Editor } from "@edcoreorg/core"
+const edcore = require("@edcoreorg/core");
+
+// Create an instance of the editor
+const editor = new edcore.Editor();
+
+// Insert some text
+editor.insert_text(0, "Hello, Flex!\nThis is Edcore in action.");
+
+// Print the first line
+console.log("Line 0:", editor.get_line(0));
+
+// Show how many lines exist
+console.log("Total lines:", editor.line_count());
+
+// Optional: test your on_change handler (if implemented)
+if (editor.on_change) {
+  editor.on_change((text) => {
+    console.log("Buffer changed:", text);
+  });
+
+  editor.insert_text(11, "\nThis triggered the on_change callback!");
+}
