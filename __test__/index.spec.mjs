d8efa642ec3d22dafb9e40a8b19cd370480a0ec0
Mani Arasteh <mani.arasteh92@gmail.com>
2025-11-12 20:38:51 +0330

    Update index.spec.mjs

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,25 @@
-import test from 'ava'
+import test from "ava";
+import { Editor } from "../index.js"; // this points to your napi-built module
 
-import { sum } from '../index.js'
+test("Editor can be created", (t) => {
+  const editor = new Editor();
+  t.truthy(editor);
+});
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test("Editor inserts and reads text", (t) => {
+  const editor = new Editor();
+  editor.insert_text(0, "Hello, Edcore!");
+  const line = editor.get_line(0);
+  t.is(line, "Hello, Edcore!");
+});
+
+test("Editor line count works", (t) => {
+  const editor = new Editor();
+  editor.insert_text(0, "Line 1\nLine 2\nLine 3");
+  t.is(editor.line_count(), 3);
+});
+
+test("Editor returns empty string for out-of-bounds line", (t) => {
+  const editor = new Editor();
+  t.is(editor.get_line(42), "");
+});
